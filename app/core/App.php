4a07ce02c49571482b7e34b4f32139634ee9c962
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-19 13:50:45 +0700

    explode url

--- app/core/App.php
+++ app/core/App.php
@@ -12,6 +12,8 @@ class App
     {
         if (isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
