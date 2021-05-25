11ce91c48f05d35a5a05998350c5afe3b45ea3b0
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-25 08:02:04 +0700

    tambah css bootsrap

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->judul; ?></title>
+    <link rel="stylesheet" href="http://localhost/belajarmvc/public/css/bootstrap.css">
 </head>
 
 <body>
