87db2dbc04613e3bedb1c8b7e2ca33965c696b60
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-25 15:59:00 +0700

    model data dari array

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -5,8 +5,10 @@ class Mahasiswa extends Controller
     public $judul = __CLASS__;
     public function index()
     {
+        $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
+
         $this->view('templates/header');
-        $this->view('mahasiswa/index');
+        $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
     }
 }
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,5 +1,31 @@
 <?php 
 
-class 
+class Mahasiswa_model {
+    private $mhs = [
+        [
+            'nama' => 'Eryawan',
+            'nrp' => '152016107',
+            'email' => 'eryawan@gmail.com',
+            'jurusan' => 'Teknik Informatika'
+        ],
+        [
+            'nama' => 'Hasan bin Ali',
+            'nrp' => '1420019',
+            'email' => 'hasan@gmail.com',
+            'jurusan' => 'Teknik Akhlaq'
+        ],
+        [
+            'nama' => 'Husein bin Ali',
+            'nrp' => '239911',
+            'email' => 'husein@gmail.com',
+            'jurusan' => 'Teknik Mesin'
+        ]
+
+    ];
+
+    public function getAllMahasiswa(){
+        return $this->mhs;
+    }
+}
 
 ?>
