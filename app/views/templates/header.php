2491222e91a4f5deb1c3dc87f68f062798df1b01
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-25 09:42:51 +0700

    edit view

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -10,3 +10,17 @@
 </head>
 
 <body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand font-weight-bold" href="<?= BASEURL; ?>">MvC</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link <?= $this->judul == 'Home' ? 'active font-weight-bold' : ''; ?>" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link  <?= $this->judul == 'About' ? 'active font-weight-bold' : ''; ?>" href="<?= BASEURL . '/about'; ?>">About</a>
+                </div>
+            </div>
+    </div>
+    </nav>
